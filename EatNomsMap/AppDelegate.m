218b11770b2d8ec522291ac0d5babddbaee150cb
Don Bertamini <donbertamini@gmail.com>
2015-06-20 11:24:23 -0700

    just setup parse frameworks

--- EatNomsMap/AppDelegate.m
+++ EatNomsMap/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
    // Override point for customization after application launch.
+   
+   
+   [Parse setApplicationId:@"Z4pStH9Hx0bBkV18vkt4o8wRznPHoWBNe1N8wIbQ"
+                 clientKey:@"8eVDmN5iJVa8H8Mb5eyvWnR1JBkIthzVSWY7ve1g"];
+
    return YES;
 }
 
